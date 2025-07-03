72ab4a21d3d5baf53c0f8e12ff727965c539f036
Plixelated <76276169+Plixelated@users.noreply.github.com>
2025-07-03 11:27:13 -0700

    chore: formatting

--- models/uploaded_files_model.py
+++ models/uploaded_files_model.py
@@ -8,61 +8,31 @@ from .study_config_model import StudyConfiguration
 
 
 class UploadedFiles(Base):
-    __tablename__="files_config"
+    __tablename__ = "files_config"
 
-    #STUDY CONFIG ID (FK,PK)
-    study_config_id:Mapped[uuid.UUID] = mapped_column(
+    # STUDY CONFIG ID (FK,PK)
+    study_config_id: Mapped[uuid.UUID] = mapped_column(
         UUID(as_uuid=True),
-        ForeignKey(
-            "study_config.id",
-            ondelete="CASCADE",
-            onupdate="CASCADE"),
+        ForeignKey("study_config.id", ondelete="CASCADE", onupdate="CASCADE"),
         primary_key=True,
-        unique=True
+        unique=True,
     )
 
-    consent_form:Mapped[str] = mapped_column(
-        String,
-        nullable=False
-    )
+    consent_form: Mapped[str] = mapped_column(String, nullable=False)
 
-    consent_form_bytes:Mapped[BYTEA] = mapped_column(
-        BYTEA,
-        nullable=False
-    )    
+    consent_form_bytes: Mapped[BYTEA] = mapped_column(BYTEA, nullable=False)
 
-    learning_image_list:Mapped[str] = mapped_column(
-        String,
-        nullable=False
-    )
+    learning_image_list: Mapped[str] = mapped_column(String, nullable=False)
 
-    experiment_image_list:Mapped[str] = mapped_column(
-        String,
-        nullable=False
-    )
+    experiment_image_list: Mapped[str] = mapped_column(String, nullable=False)
 
-    study_instructions:Mapped[str] = mapped_column(
-        String,
-        nullable=False
-    )
+    study_instructions: Mapped[str] = mapped_column(String, nullable=False)
 
-    study_instructions_bytes:Mapped[BYTEA] = mapped_column(
-        BYTEA,
-        nullable=False
-    )    
+    study_instructions_bytes: Mapped[BYTEA] = mapped_column(BYTEA, nullable=False)
 
-    study_debrief:Mapped[str] = mapped_column(
-        String,
-        nullable=False
-    )
+    study_debrief: Mapped[str] = mapped_column(String, nullable=False)
 
-    study_debrief_bytes:Mapped[BYTEA] = mapped_column(
-        BYTEA,
-        nullable=False
-    )    
+    study_debrief_bytes: Mapped[BYTEA] = mapped_column(BYTEA, nullable=False)
 
-
-
-    #REFERENCE TO STUDY CONFIG
-    study : Mapped[StudyConfiguration]=relationship(back_populates="files")
-
+    # REFERENCE TO STUDY CONFIG
+    study: Mapped[StudyConfiguration] = relationship(back_populates="files")
