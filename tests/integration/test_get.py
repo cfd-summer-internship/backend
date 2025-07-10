c574dcb5daa801e07b3ad10d351f7562e21904d0
chrmourad <cristymrad05@gmail.com>
2025-07-09 19:07:31 -0700

    Individual API calls for each phase

--- tests/integration/test_get.py
+++ tests/integration/test_get.py
@@ -21,3 +21,52 @@ async def test_get_study_config():
         json_data = response.json()
         parsed = StudyConfigResponse(**json_data)
         assert parsed
+
+@pytest.mark.asyncio
+async def test_get_learning_phase():
+    async with AsyncClient(
+        transport=ASGITransport(app=app), base_url="http://test"
+    ) as client:
+        study_response = await client.get("/study/study_ids")
+        assert study_response.status_code == 200
+        study_id = study_response.json()
+
+        response = await client.get(f"/study/learning_phase/{study_id[0]}")
+        assert response.status_code == 200
+        json_data = response.json()
+        assert "display_duration" in json_data
+        assert "pause_duration" in json_data
+        assert "display_method" in json_data
+
+
+@pytest.mark.asyncio
+async def test_get_waiting_phase():
+    async with AsyncClient(
+        transport=ASGITransport(app=app), base_url="http://test"
+    ) as client:
+        study_response = await client.get("/study/study_ids")
+        assert study_response.status_code == 200
+        study_id = study_response.json()
+
+        response = await client.get(f"/study/waiting_phase/{study_id[0]}")
+        assert response.status_code == 200
+        json_data = response.json()
+        assert "display_duration" in json_data
+
+
+@pytest.mark.asyncio
+async def test_get_experiment_phase():
+    async with AsyncClient(
+        transport=ASGITransport(app=app), base_url="http://test"
+    ) as client:
+        study_response = await client.get("/study/study_ids")
+        assert study_response.status_code == 200
+        study_id = study_response.json()
+
+        response = await client.get(f"/study/experiment_phase/{study_id[0]}")
+        assert response.status_code == 200
+        json_data = response.json()
+        assert "display_duration" in json_data
+        assert "pause_duration" in json_data
+        assert "display_method" in json_data
+        assert "response_method" in json_data
