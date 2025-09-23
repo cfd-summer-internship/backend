d2cb283e6441d07abe31690793513e8118b6321d
Mia Fraser <mia.jaylen@proton.me>
2025-09-23 19:54:32 +0000

    fix: search requests

--- routes/staff_routes.py
+++ routes/staff_routes.py
@@ -6,36 +6,37 @@ from db.client import get_db_session
 from models.enums import UserRole
 from models.user_model import User
 from schemas.researcher_dashboard_schema import StudyResultsSchema
+from schemas.staff_schemas import SearchRequest
 from services.researcher_dashboard_service import get_all_study_results, get_researcher_id, get_study_codes
 
 
 router = APIRouter(prefix="/staff", tags=["Staff"])
 # STAFF
-@router.get("/search")
+@router.post("/search")
 async def get_researcher_by_email(
-    email: str,
+    payload : SearchRequest,
     user: User = Depends(require_role(UserRole.STAFF)),
     conn: AsyncSession = Depends(get_db_session),
 ):
-    return await get_researcher_id(email, conn)
+    return await get_researcher_id(payload.email, conn)
 
 
-@router.get("/search/configs")
+@router.post("/search/configs")
 async def get_researcher_configs(
-    email: str,
+    payload : SearchRequest,
     user: User = Depends(require_role(UserRole.STAFF)),
     conn: AsyncSession = Depends(get_db_session),
 ):
-    researcher_id = await get_researcher_id(email, conn)
+    researcher_id = await get_researcher_id(payload.email, conn)
     study_codes = await get_study_codes(conn, researcher_id)
     return {"study_codes": study_codes}
 
 
-@router.get("/search/results")
+@router.post("/search/results")
 async def get_researcher_results(
-    email: str,
+    payload : SearchRequest,
     user: User = Depends(require_role(UserRole.STAFF)),
     conn: AsyncSession = Depends(get_db_session),
 ) -> list[StudyResultsSchema]:
-    researcher_id = await get_researcher_id(email, conn)
+    researcher_id = await get_researcher_id(payload.email, conn)
     return await get_all_study_results(researcher_id, conn)
